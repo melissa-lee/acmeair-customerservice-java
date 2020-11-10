5b6ce5dea74068c6d973b0e124919db9e2b70e9d
jdmcclur <jdmcclur@us.ibm.com>
2020-11-10 15:55:13 -0600

    add more tests

--- src/test/java/it/com/acmeair/tests/CustomerServiceTests.java
+++ src/test/java/it/com/acmeair/tests/CustomerServiceTests.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.text.ParseException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -31,7 +32,10 @@ import de.flapdoodle.embed.process.runtime.Network;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -50,10 +54,26 @@ public class CustomerServiceTests {
   private static String BASE_URL;
   private static String BASE_URL_WITH_CONTEXT_ROOT;
 
-  private static final String PING_ENDPOINT = "/";
+  private static final String USERNAME = "uid0@email.com";
+  private static final String PASSWORD = "password";
 
+  private static final String LOAD_ENDPOINT = "/loader/load?numCustomers=10000";
+  private static final String LOAD_RESPONSE =  "Loaded 10000 customers in ";
+
+  private static final String PING_ENDPOINT = "/";
   private static final String PONG_RESPONSE = "OK";
-  private static final String LOAD_RESPONSE =  "Loaded flights for 5 days in";
+
+  private static final String VALIDATE_ENDPOINT = "/internal/validateid";
+  private static final String VALIDATE_RESPONSE = "{\"validated\":true}";
+  private static final String VALIDATE_BAD_RESPONSE = "{\"validated\":false}";
+
+  private static final String UPDATE_MILES_ENDPOINT = "/internal/updateCustomerTotalMiles/uid0@email.com";
+  private static final String UPDATE_MILES_RESPONSE = "{\"miles\":10}";
+
+  private static final String GET_CUSTOMER_ENDPOINT = "/byid/uid0@email.com";
+  private static final String CUSTOMER_UPDATE = "{ \"_id\" : \"" + USERNAME + "\", \"status\" : \"GOLD\", \"total_miles\" : 1000000, \"miles_ytd\" : 1000, \"address\" : { \"streetAddress1\" : \"123 Main St.\", \"streetAddress2\" : \"NOTNULL\", \"city\" : \"Anytown\", \"stateProvince\" : \"NC\", \"country\" : \"USA\", \"postalCode\" : \"27617\" }, \"phoneNumber\" : \"919-123-4567\", \"phoneNumberType\" : \"BUSINESS\", \"password\" : \"" + PASSWORD + "\" }";
+
+  private static final String GET_CUSTOMER_RESPONSE = ""; 
 
   private static final String HEALTH_ENDPOINT="/health";
   private static final String HEALTH_RESPONSE="],\"status\":\"UP\"}";
@@ -106,23 +126,102 @@ Version.Main.PRODUCTION.getFeatures());
     } 
   }
 
+  @Test   
+  public void test1_Load() {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + LOAD_ENDPOINT; 
+    doTest(url,Status.OK,LOAD_RESPONSE);
+  }
+
   @Test
-  public void test1_Ping() {
+  public void test2_Ping() {
     String url = BASE_URL_WITH_CONTEXT_ROOT + PING_ENDPOINT; 
     doTest(url,Status.OK,PONG_RESPONSE);
   }
 
   @Test   
-  public void test2_Health() {
+  public void test3_Health() {
     String url = BASE_URL + HEALTH_ENDPOINT; 
     doTest(url,Status.OK,HEALTH_RESPONSE);
   }
 
   //@Test   
   // openapi does not work in loose config mode
-  public void test3_OpenAPI() {
+  public void test4_OpenAPI() {
     String url = BASE_URL + OPENAPI_ENDPOINT; 
     doTest(url,Status.OK,LOAD_RESPONSE);
+  }
+
+  @Test
+  public void test5_Validate() throws InterruptedException {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + VALIDATE_ENDPOINT; 
+    
+    WebTarget target = client.target(url);
+     
+    Form form = new Form();
+    form.param("login", "uid0@email.com");
+    form.param("password", "password");
+
+
+    Response response = target.request().post(Entity.entity(form,MediaType.APPLICATION_FORM_URLENCODED_TYPE),Response.class);
+    Thread.sleep(20);
+    assertEquals("Incorrect response code from " + url, 
+      Status.OK.getStatusCode(), response.getStatus());
+
+    
+    String result = response.readEntity(String.class);
+    assertThat(result, containsString(VALIDATE_RESPONSE));
+        
+    response.close();
+  }
+
+  @Test
+  public void test5_ValidateBad() throws InterruptedException {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + VALIDATE_ENDPOINT; 
+    
+    WebTarget target = client.target(url);
+     
+    Form form = new Form();
+    form.param("login", "uid0@email.com");
+    form.param("password", "passord");
+
+
+    Response response = target.request().post(Entity.entity(form,MediaType.APPLICATION_FORM_URLENCODED_TYPE),Response.class);
+    Thread.sleep(20);
+    assertEquals("Incorrect response code from " + url, 
+      Status.OK.getStatusCode(), response.getStatus());
+
+    
+    String result = response.readEntity(String.class);
+    assertThat(result, containsString(VALIDATE_BAD_RESPONSE));
+        
+    response.close();
+  }
+
+  @Test
+  public void test6_UpdateMiles() throws InterruptedException {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + UPDATE_MILES_ENDPOINT; 
+    
+    WebTarget target = client.target(url);
+     
+    Form form = new Form();
+    form.param("miles", "10");
+    
+    Response response = target.request().post(Entity.entity(form,MediaType.APPLICATION_FORM_URLENCODED_TYPE),Response.class);
+    Thread.sleep(20);
+    assertEquals("Incorrect response code from " + url, 
+      Status.OK.getStatusCode(), response.getStatus());
+
+    
+    String result = response.readEntity(String.class);
+    assertThat(result, containsString(UPDATE_MILES_RESPONSE));
+        
+    response.close();
+  }
+
+  @Test
+  public void test7_GetCustomer() {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + GET_CUSTOMER_ENDPOINT; 
+    doTest(url,Status.UNAUTHORIZED,GET_CUSTOMER_RESPONSE);
   }
      
   private void doTest(String url, Status status, String expectedResponse) {
@@ -137,6 +236,21 @@ Version.Main.PRODUCTION.getFeatures());
       assertThat(result, containsString(expectedResponse));
     }
     
+    response.close();
+  }
+
+  @Test
+  public void test08_updateCustomer() throws InterruptedException, ParseException {
+    String url = BASE_URL_WITH_CONTEXT_ROOT + GET_CUSTOMER_ENDPOINT; 
+    
+    WebTarget target = client.target(url);
+     
+    Response response = target.request().post(Entity.entity(CUSTOMER_UPDATE,MediaType.APPLICATION_JSON),Response.class);
+
+    Thread.sleep(20);
+    assertEquals("Incorrect response code from " + url, 
+      Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
+            
     response.close();
   }
 
